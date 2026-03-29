74df6f0e1e0d9736f2d0491b5262e6858d612297
Abdulgaffar Bello <abdulgaffarbello@Abdulgaffars-MacBook-Pro.local>
2026-03-29 01:54:45 +0100

    Fix Express 5 wildcard route PathError

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use('/uploads', express.static('uploads'));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
     });
 } else {
